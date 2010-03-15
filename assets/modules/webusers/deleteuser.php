539d9b96940ffdeee9811a75ccf0df31a3f45d59
giel.berkers <giel.berkers@895042f4-a235-11de-b0cc-ab0ad15e60d0>
2010-03-15 15:19:49 +0000

    deleted native plugins and modules-folders

--- assets/modules/webusers/deleteuser.php
+++
@@ -1,7 +0,0 @@
-<?php
-	if(isset($parameters[1])) {
-		$this->db->delete('webusers', array('id'=>$parameters[1]));
-		$this->db->delete('webusers_user_group', array('id_user'=>$parameters[1]));
-		redirect(moduleCreateLink(array('webusers')));
-	}
-?>
